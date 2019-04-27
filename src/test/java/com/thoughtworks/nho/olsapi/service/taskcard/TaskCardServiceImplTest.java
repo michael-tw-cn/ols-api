9280de5670231eb129f180c63c1a8c3004fc5d0c
liyanmin <13824774439@163.com>
2019-04-27 22:56:37 +0800

    add TaskCardService test

--- src/test/java/com/thoughtworks/nho/olsapi/service/taskcard/TaskCardServiceImplTest.java
+++ src/test/java/com/thoughtworks/nho/olsapi/service/taskcard/TaskCardServiceImplTest.java
@@ -4,8 +4,6 @@ import com.thoughtworks.nho.olsapi.entity.TaskCard;
 import com.thoughtworks.nho.olsapi.repository.TaskCardRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,7 +12,6 @@ import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
