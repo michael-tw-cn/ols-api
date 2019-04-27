aab8daa1fd4261b614431878ecf7ed00d9794fcc
Xin Leng <xin.leng@thoughtworks.com>
2019-04-27 17:26:05 +0800

    add exercise

---
+++ src/main/java/com/thoughtworks/nho/olsapi/item/Exercise.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.nho.olsapi.item;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Exercise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String value;
+
+    private int score;
+
+
+}
