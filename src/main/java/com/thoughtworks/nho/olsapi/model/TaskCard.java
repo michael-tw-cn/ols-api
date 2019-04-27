89545adeb38b69ceb8b0e46c8accb1bb6b963821
liyanmin <13824774439@163.com>
2019-04-27 18:47:59 +0800

    add TaskCardController

--- src/main/java/com/thoughtworks/nho/olsapi/model/TaskCard.java
+++ src/main/java/com/thoughtworks/nho/olsapi/model/TaskCard.java
@@ -1,7 +1,35 @@
 package com.thoughtworks.nho.olsapi.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class TaskCard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    //todo: 题目
+//    private Topic topic;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/com/thoughtworks/nho/olsapi/repository/TaskCardRepository.java
+++ src/main/java/com/thoughtworks/nho/olsapi/repository/TaskCardRepository.java
@@ -3,5 +3,5 @@ package com.thoughtworks.nho.olsapi.repository;
 import com.thoughtworks.nho.olsapi.model.TaskCard;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TaskCardRepository extends CrudRepository<TaskCard, Long> {
+public interface TaskCardRepository extends CrudRepository<TaskCard, Integer> {
 }
