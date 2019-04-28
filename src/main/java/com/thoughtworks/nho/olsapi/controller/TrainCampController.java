d20088f524aa4d42a9b7202c7b4d6cfc97fe77ef
kee <keeyang108@163.com>
2019-04-28 15:21:07 +0800

    TrainCampController 支持跨域

--- src/main/java/com/thoughtworks/nho/olsapi/controller/TrainCampController.java
+++ src/main/java/com/thoughtworks/nho/olsapi/controller/TrainCampController.java
@@ -8,16 +8,14 @@ import com.thoughtworks.nho.olsapi.util.WebResponseBuilder;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Api("训练营服务")
 @RestController
 @RequestMapping("/v1/traincamp")
+@CrossOrigin 
 public class TrainCampController {
 
     @Autowired
